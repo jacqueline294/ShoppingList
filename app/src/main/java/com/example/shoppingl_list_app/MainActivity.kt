09a83fc2e00e57d6ee9821baba351f20795cca2f
jacqueline294 <jngigi86@gmail.com>
2024-04-26 10:21:26 +0200

    first commit

---
+++ app/src/main/java/com/example/shoppingl_list_app/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.shoppingl_list_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.shoppingl_list_app.ui.theme.Shoppingl_list_appTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Shoppingl_list_appTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
